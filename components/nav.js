d5ff7e7b69ecc2116c55c7153223c921de3ff53c
Quinn Salas <qmsalas321@gmail.com>
2020-09-06 16:01:00 -0500

    style: add feather icons ✨

--- components/nav.js
+++ components/nav.js
@@ -1,14 +1,27 @@
+import { Home, Edit, Camera, GitHub, Instagram, Mail } from 'react-feather';
 import styles from '../styles/nav.module.css';
 
 const Nav = () => (
 	<nav className={styles.nav}>
 		<ul>
-			<li> Home </li>
-			<li> Blog </li>
-			<li> Photo </li>
-			<li> Github </li>
-			<li> Instagram </li>
-			<li> Email </li>
+			<li>
+				<Home />
+			</li>
+			<li>
+				<Edit />
+			</li>
+			<li>
+				<Camera />
+			</li>
+			<li>
+				<GitHub />
+			</li>
+			<li>
+				<Instagram />
+			</li>
+			<li>
+				<Mail />
+			</li>
 		</ul>
 	</nav>
 );
