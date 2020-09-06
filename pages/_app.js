fce8b20e7e406861f70fc6980f97e478d3066658
Quinn Salas <qmsalas321@gmail.com>
2020-09-06 16:28:25 -0500

    add prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.element.isRequired,
+  pageProps: PropTypes.shape({}).isRequired
+};
+
+export default MyApp;
