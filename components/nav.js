f5549ea1d6b3836fbd028ba377bf39bd693011a3
Quinn Salas <qmsalas321@gmail.com>
2020-09-06 16:48:11 -0500

    lint

--- components/nav.js
+++ components/nav.js
@@ -2,28 +2,28 @@ import { Home, Edit, Camera, GitHub, Instagram, Mail } from 'react-feather';
 import styles from '../styles/nav.module.css';
 
 const Nav = () => (
-	<nav className={styles.nav}>
-		<ul>
-			<li>
-				<Home />
-			</li>
-			<li>
-				<Edit />
-			</li>
-			<li>
-				<Camera />
-			</li>
-			<li>
-				<GitHub />
-			</li>
-			<li>
-				<Instagram />
-			</li>
-			<li>
-				<Mail />
-			</li>
-		</ul>
-	</nav>
+  <nav className={styles.nav}>
+    <ul>
+      <li>
+        <Home />
+      </li>
+      <li>
+        <Edit />
+      </li>
+      <li>
+        <Camera />
+      </li>
+      <li>
+        <GitHub />
+      </li>
+      <li>
+        <Instagram />
+      </li>
+      <li>
+        <Mail />
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Nav;
