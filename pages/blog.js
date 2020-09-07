58dc5d352b2f4a9ae885589191378879d855ccd9
Quinn Salas <qmsalas321@gmail.com>
2020-09-07 13:10:18 -0500

    render posts on blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,11 @@
+import PropTypes from 'prop-types';
 import Head from 'next/head';
-import Nav from '../components/nav';
+import Nav from '../components/Nav';
+import PostList from '../components/PostList';
+import getPosts from '../util/getPosts';
 import styles from '../styles/blog.module.css';
 
-const Blog = () => (
+const Blog = ({ posts }) => (
   <div className={styles.container}>
     <Head>
       <title>Quinn Salas</title>
@@ -19,8 +22,25 @@ const Blog = () => (
 
     <main className={styles.container}>
       <h1> Welcome to my blog</h1>
+      <PostList posts={posts} />
     </main>
   </div>
 );
+
+export const getStaticProps = async () => {
+  const posts = ((context) => {
+    return getPosts(context);
+  })(require.context('../posts', true, /\.md$/));
+
+  return {
+    props: {
+      posts
+    }
+  };
+};
+
+Blog.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.object).isRequired
+};
 
 export default Blog;
