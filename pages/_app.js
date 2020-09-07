cd635dae5de20be31c66c394d01f2ae6c8a38c81
Quinn Salas <qmsalas321@gmail.com>
2020-09-07 11:15:09 -0500

    add animations between pg transitions cleanup some PropTypes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps, router }) => (
+  <motion.div
+    key={router.pathname}
+    initial="pageInitial"
+    animate="pageAnimate"
+    variants={{
+      pageInitial: {
+        opacity: 0
+      },
+      pageAnimate: {
+        opacity: 1
+      }
+    }}
+  >
+    <Component {...pageProps} />
+  </motion.div>
+);
 
 MyApp.propTypes = {
-  Component: PropTypes.element.isRequired,
-  pageProps: PropTypes.shape({}).isRequired
+  Component: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
+  pageProps: PropTypes.shape({}).isRequired,
+  router: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired
 };
 
 export default MyApp;
