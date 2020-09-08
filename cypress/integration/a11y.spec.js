d506926695e27523b802777456b78e176d759b9c
Quinn Salas <qmsalas321@gmail.com>
2020-09-08 00:15:09 -0500

    feat: init a11y int test

---
+++ cypress/integration/a11y.spec.js
@@ -0,0 +1,10 @@
+describe('a11y', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+    cy.injectAxe();
+  });
+
+  it('should not have a11y errors', () => {
+    cy.checkA11y();
+  });
+});
