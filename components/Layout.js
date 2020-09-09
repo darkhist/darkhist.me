7200d8573ceda485b6aa93bb1f3e53709ef672b9
Quinn Salas <qmsalas321@gmail.com>
2020-09-09 11:00:32 -0500

    feat: emoji favicon

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,10 @@ const Layout = ({ children }) => (
     <Head>
       <title>Quinn Salas</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎱</text></svg>"
+      />
     </Head>
 
     <header>
