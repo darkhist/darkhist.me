9f0ab77e1c6dd7257f69a06750ffd85dd67f3826
Quinn Salas <qmsalas321@gmail.com>
2020-09-08 19:54:34 -0500

    style: prefer Inter import over using Google Fonts enable font-feature settings

--- components/Layout.js
+++ components/Layout.js
@@ -9,10 +9,6 @@ const Layout = ({ children }) => (
       <title>Quinn Salas</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap"
-        rel="stylesheet"
-      />
     </Head>
 
     <header>
