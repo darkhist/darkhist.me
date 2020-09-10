beefa100c48c01e855da435b1319f5d30b86b4b6
Quinn Salas <qmsalas321@gmail.com>
2020-09-09 21:14:01 -0500

    int: moar tests

--- cypress/integration/a11y.spec.js
+++ cypress/integration/a11y.spec.js
@@ -8,3 +8,25 @@ describe('/', () => {
     cy.checkA11y();
   });
 });
+
+describe('/blog', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/blog');
+    cy.injectAxe();
+  });
+
+  it('should not have a11y errors', () => {
+    cy.checkA11y();
+  });
+});
+
+describe('/[post]', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/blog/cypress-axe-part-one');
+    cy.injectAxe();
+  });
+
+  it('should not have a11y errors', () => {
+    cy.checkA11y();
+  });
+});
